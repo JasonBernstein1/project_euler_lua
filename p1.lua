8147b494e08aa38ac4294eff60669429c276b630
JasonBernstein1 <jstatbernstein@gmail.com>
2025-05-02 21:54:57 -0600

    Add solution for problem 1

---
+++ p1.lua
@@ -0,0 +1,23 @@
+#!/usr/bin/env lua
+
+--[[
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of
+all the multiples of 3 or 5 below 1000.
+--]]
+
+--- Sum multiples of 3 or 5 below n.
+---@param n number The integer upper bound, equal to 1,000 for this problem.
+local function sum_integers(n)
+    local total=0
+    for i = 1,n do
+        if i % 3 == 0 or i % 5 == 0 then
+            total = total + i
+        end
+    end
+    return total
+end
+
+-- Compute solution
+local n = 999
+print(sum_integers(n)) -- 233,168
